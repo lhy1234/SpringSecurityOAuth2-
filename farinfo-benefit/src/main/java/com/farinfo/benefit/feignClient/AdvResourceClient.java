7233aa19a60188f9d11af32e334f7ecdd98ef7ee
李浩洋 <13298457669@163.com>
2020-05-06 17:42:41 +0800

    删除了没用得公益

--- farinfo-benefit/src/main/java/com/farinfo/benefit/feignClient/AdvResourceClient.java
+++
@@ -1,9 +0,0 @@
-package com.farinfo.benefit.feignClient;
-
-import org.springframework.cloud.openfeign.FeignClient;
-
-/**
- * Created by: 李浩洋 on 2020-04-30
- **/
-public interface AdvResourceClient {
-}
